44cbad3354721c08a644ad178dc56e64e89c7a49
berylcl <gakiiberyl@gmail.com>
2023-06-13 21:15:56 +0100

    using redux store

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,7 +2,27 @@
 
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
+// Initial state
+const initialState = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
 
 const booksSlice = createSlice({
   name: 'books',
